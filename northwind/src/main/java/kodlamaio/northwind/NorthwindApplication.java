83b30d6c6737f6aa27abf3b77fa86eba7be81a3b
Berkcan Altungöz <61842758+BerkcanAltungoz@users.noreply.github.com>
2022-05-19 03:44:13 +0300

    swagger fix - spring boot downgraded

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -3,7 +3,6 @@ package kodlamaio.northwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,8 +20,7 @@ public class NorthwindApplication {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))
                 .build();
     }
 
