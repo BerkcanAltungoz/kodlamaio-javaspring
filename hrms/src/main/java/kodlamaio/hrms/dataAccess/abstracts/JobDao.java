c865900656291ba1f32eadad28fcef3a259240a3
Berkcan Altungöz <61842758+BerkcanAltungoz@users.noreply.github.com>
2022-05-28 18:29:06 +0300

    Update JobDao.java

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobDao.java
@@ -2,6 +2,8 @@ package kodlamaio.hrms.dataAccess.abstracts;
 
 import kodlamaio.hrms.entities.concretes.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobDao extends JpaRepository<Job,Integer> {
 }
